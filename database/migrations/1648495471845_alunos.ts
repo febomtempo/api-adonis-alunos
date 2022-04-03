3f4d320e3a6e597f7c1f2ed3096df16908c2fc93
febomtempo <febomtempo@hotmail.com>
2022-04-03 00:19:49 -0300

    agora vai part.2

--- database/migrations/1648495471845_alunos.ts
+++ database/migrations/1648495471845_alunos.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class Alunos extends BaseSchema {
-  protected tableName = 'alunos-bd'
+  protected tableName = 'alunos'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
